c74784c406ffc323d743bb0ef5c0cd5adac6027e
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-02-19 11:23:58 +0100

    Functions

---
+++ functions.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Houses - Functions
+ *
+ * @package Coordinator\Modules\Houses
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+
+ // include classes
+ require_once(DIR."modules/houses/classes/cHousesHouse.class.php");
+
+?>
