4692182160dca812268342bed8c6fedf33d2366b
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-02-19 15:11:11 +0100

    Houses template

--- template.inc.php
+++ template.inc.php
@@ -13,6 +13,19 @@
  $nav->setTitle(api_text(MODULE));
  // dashboard
  $nav->addItem(api_icon("fa-th-large",null,"hidden-link"),api_url(["scr"=>"dashboard"]));
+ // houses
+ if(api_script_prefix()=="houses"){
+  $nav->addItem(api_text("nav-houses-list"),api_url(["scr"=>"houses_list"]));
+  // operations
+  if($house_obj->id && in_array(SCRIPT,array("houses_view","houses_edit"))){
+   $nav->addItem(api_text("nav-operations"),null,null,"active");
+   $nav->addSubItem(api_text("nav-houses-operations-edit"),api_url(["scr"=>"houses_edit","idHouse"=>$house_obj->id]),(api_checkAuthorization("houses-houses_manage")));
+   $nav->addSubSeparator();
+   $nav->addSubItem(api_text("nav-houses-operations-room_add"),api_url(["scr"=>"houses_view","tab"=>"rooms","act"=>"room_add","idHouse"=>$house_obj->id]),(api_checkAuthorization("houses-houses_manage")));
+  }else{
+   $nav->addItem(api_text("nav-houses-add"),api_url(["scr"=>"houses_edit"]),(api_checkAuthorization("houses-houses_manage")));
+  }
+ }
  // add nav to html
  $app->addContent($nav->render(false));
 ?>
