858141c1b0053bce0a6226afbdc40c3650609342
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-02-19 16:33:56 +0100

    House class update

--- classes/cHousesHouse.class.php
+++ classes/cHousesHouse.class.php
@@ -30,6 +30,26 @@
   protected $country;
 
   /**
+   * Decode log properties
+   *
+   * {@inheritdoc}
+   */
+  public static function log_decode($event,$properties){
+   api_dump($event);
+   // make return array
+   $return_array=array();
+   // users events
+   if($properties['class']=="cUser"){$return_array[]=api_text("cUser").": ".(new cUser($properties['id']))->fullname;}
+   // rooms events
+   if($properties['idRoom']){
+    $return_array[]=api_text("cHousesHouseRoom").": ".(new cHousesHouseRoom($properties['idRoom']))->name;
+    if($properties['name']){$return_array[]=api_text("cHousesHouseRoom-property-name").": ".$properties['name']['previous']." &rarr; ".$properties['name']['current'];}
+   }
+   // return
+   return implode(" | ",$return_array);
+  }
+
+  /**
    * Get Label
    *
    * @return string House label
@@ -67,42 +87,105 @@
    return $address;
   }
 
- /**
-  * Edit form
-  *
-  * @param string[] $additional_parameters Array of url additional parameters
-  * @return object Form structure
-  */
- public function form_edit(array $additional_parameters=null){
-  // build form
-  $form=new strForm("?mod=houses&scr=submit&act=house_store&idHouse=".$this->id."&".http_build_query($additional_parameters),"POST",null,null,"houses_houses_form_edit");
-  // fields
-  $form->addField("text","name",api_text("cHousesHouse-ff-name"),$this->name,api_text("cHousesHouse-ff-name-placeholder"),null,null,null,"required");
-  $form->addField("textarea","description",api_text("cHousesHouse-ff-description"),$this->description,api_text("cHousesHouse-ff-description-placeholder"),null,null,null,"rows='2'");
-  $form->addField("separator");
-  $form->addField("text","street",api_text("cHousesHouse-ff-street"),$this->street,api_text("cHousesHouse-ff-street-placeholder"),null,null,null,"required");
-  $form->addField("text","number",api_text("cHousesHouse-ff-number"),$this->number,api_text("cHousesHouse-ff-number-placeholder"));
-  $form->addField("text","internal",api_text("cHousesHouse-ff-internal"),$this->internal,api_text("cHousesHouse-ff-internal-placeholder"));
-  $form->addField("text","zip",api_text("cHousesHouse-ff-zip"),$this->zip,api_text("cHousesHouse-ff-zip-placeholder",null,null,null,"required"));
-  $form->addField("text","town",api_text("cHousesHouse-ff-town"),$this->town,api_text("cHousesHouse-ff-town-placeholder",null,null,null,"required"));
-  $form->addField("text","district",api_text("cHousesHouse-ff-district"),$this->district,api_text("cHousesHouse-ff-district-placeholder",null,null,null,"required"));
-  $form->addField("text","country",api_text("cHousesHouse-ff-country"),$this->country,api_text("cHousesHouse-ff-country-placeholder",null,null,null,"required"));  /** @todo valutare select */
-  // controls
-  $form->addControl("submit",api_text("form-fc-submit"));
-  // return
-  return $form;
- }
+  /**
+   * Get Users
+   *
+   * @return objects[] Users array sorted by fullname
+   */
+  public function getUsers(){return api_sortObjectsArray($this->joined_select("houses__houses__join__users","fkHouse","cUser","fkUser"),"fullname");}
 
- /**
-  * Disable remove function
-  */
- public function remove(){
-   /** @todo check per vedere se è vuoto e permetterne l'eliminazione */
-  throw new Exception("Remove function disabled by developer..");
- }
+  /**
+   * Get Rooms
+   *
+   * @return objects[] Rooms array
+   */
+  public function getRooms(){return cHousesHouseRoom::availables("`fkHouse`='".$this->id."'");}
 
- // debug
- //protected function event_triggered($event){api_dump($event,static::class." event triggered");}
+  /**
+   * Check
+   */
+  protected function check(){
+   // check properties
+   if(!strlen(trim($this->name))){throw new Exception("House name is mandatory..");}
+   if(!strlen(trim($this->street))){throw new Exception("House street is mandatory..");}
+   if(!strlen(trim($this->number))){throw new Exception("House number is mandatory..");}
+   if(!strlen(trim($this->zip))){throw new Exception("House zip is mandatory..");}
+   if(!strlen(trim($this->town))){throw new Exception("House town is mandatory..");}
+   if(!strlen(trim($this->district))){throw new Exception("House district is mandatory..");}
+   if(!strlen(trim($this->country))){throw new Exception("House country is mandatory..");}
+   // return
+   return true;
+  }
+
+  /**
+   * Edit form
+   *
+   * @param string[] $additional_parameters Array of url additional parameters
+   * @return object Form structure
+   */
+  public function form_edit(array $additional_parameters=null){
+   // build form
+   $form=new strForm(api_url(array_merge(["mod"=>"houses","scr"=>"controller","act"=>"store","obj"=>"cHousesHouse","idHouse"=>$this->id],$additional_parameters)),"POST",null,null,"houses_house_edit_form");
+   // fields
+   $form->addField("text","name",api_text("cHousesHouse-property-name"),$this->name,api_text("cHousesHouse-placeholder-name"),null,null,null,"required");
+   $form->addField("textarea","description",api_text("cHousesHouse-property-description"),$this->description,api_text("cHousesHouse-placeholder-description"),null,null,null,"rows='2'");
+   $form->addField("separator");
+   $form->addField("text","street",api_text("cHousesHouse-property-street"),$this->street,api_text("cHousesHouse-placeholder-street"),null,null,null,"required");
+   $form->addField("text","number",api_text("cHousesHouse-property-number"),$this->number,api_text("cHousesHouse-placeholder-number"),null,null,null,"required");
+   $form->addField("text","internal",api_text("cHousesHouse-property-internal"),$this->internal,api_text("cHousesHouse-placeholder-internal"));
+   $form->addField("text","zip",api_text("cHousesHouse-property-zip"),$this->zip,api_text("cHousesHouse-placeholder-zip"),null,null,null,"required");
+   $form->addField("text","town",api_text("cHousesHouse-property-town"),$this->town,api_text("cHousesHouse-placeholder-town"),null,null,null,"required");
+   $form->addField("text","district",api_text("cHousesHouse-property-district"),$this->district,api_text("cHousesHouse-placeholder-district"),null,null,null,"required");
+   $form->addField("text","country",api_text("cHousesHouse-property-country"),$this->country,api_text("cHousesHouse-placeholder-country"),null,null,null,"required");  /** @todo valutare select */
+   // controls
+   $form->addControl("submit",api_text("form-fc-submit"));
+   // return
+   return $form;
+  }
+
+  /**
+   * User form
+   *
+   * @param string[] $additional_parameters Array of url additional parameters
+   * @return object Form structure
+   */
+  public function form_user(array $additional_parameters=null){
+   // build form
+   $form=new strForm(api_url(array_merge(["mod"=>"houses","scr"=>"controller","act"=>"user_add","obj"=>"cHousesHouse","idHouse"=>$this->id],$additional_parameters)),"POST",null,null,"houses_house_user_form");
+   // fields
+   $form->addField("select","idUser",api_text("cHousesHouse-property-idUser"),null,api_text("cHousesHouse-property-idUser-select"),null,null,null,"required");
+   foreach(cUser::availables() as $user_fobj){$form->addFieldOption($user_fobj->id,$user_fobj->fullname);}
+   // controls
+   $form->addControl("submit",api_text("form-fc-submit"));
+   // return
+   return $form;
+  }
+
+  /**
+   * User Add
+   *
+   * @return boolean
+   */
+  public function user_add($object){return $this->joined_add("houses__houses__join__users","fkHouse","cUser","fkUser",$object,"user_added");}
+
+  /**
+   * User Remove
+   *
+   * @return boolean
+   */
+  public function user_remove($object){return $this->joined_remove("houses__houses__join__users","fkHouse","cUser","fkUser",$object,"user_removed");}
+
+  /**
+   * Disable remove function
+   */
+  public function remove(){
+    /** @todo check per vedere se è vuoto e permetterne l'eliminazione */
+   throw new Exception("Remove function disabled by developer..");
+  }
+
+  // debug
+  //protected function event_triggered($event){api_dump($event,static::class." event triggered");}
+
  }
 
 ?>
